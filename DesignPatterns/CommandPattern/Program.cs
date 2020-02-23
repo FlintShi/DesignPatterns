75fbb116fe53e77a21f31a4f0854745483c8a3ca
FlintShi <yan.shi@accunome.com.cn>
2020-02-23 21:02:24 +0800

    添加 命令模式和中介模式

---
+++ DesignPatterns/CommandPattern/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandPattern
+{
+class Program
+{
+	private static Invoker invoker;
+	private static Command command;
+	static void Main(string[] args)
+	{
+		//首先声明出调用者Invoker
+		invoker = new Invoker();
+		//定义一个发送给接收者的命令
+		command = new ConcreteCommand1();
+		//把命令交给调用者去执行
+		invoker.setCommand(command);
+		invoker.action();
+	}
+}
+public abstract class Command
+{
+	//定义一个子类的全局共享变量
+	protected readonly Receiver receiver;
+	//实现类必须定义一个接收者
+	public Command(Receiver _receiver)
+	{
+		this.receiver = _receiver;
+	}
+	//每个命令类都必须有一个执行命令的方法
+	public abstract void execute();
+}
+public class ConcreteCommand1 : Command
+{
+	//声明自己的默认接收者
+	public ConcreteCommand1() : base(new ConcreteReciver1())
+	{ }
+	//设置新的接收者
+	public ConcreteCommand1(Receiver _receiver) : base(_receiver)
+	{ }
+	//每个具体的命令都必须实现一个命令
+	public override void execute()
+	{
+		//业务处理
+		base.receiver.doSomething();
+	}
+}
+public class ConcreteCommand2 : Command
+{
+	//声明自己的默认接收者
+	public ConcreteCommand2() : base(new ConcreteReciver2())
+	{ }
+	//设置新的接收者
+	public ConcreteCommand2(Receiver _receiver) : base(_receiver)
+	{ }
+	//每个具体的命令都必须实现一个命令
+	public override void execute()
+	{
+		//业务处理
+		base.receiver.doSomething();
+	}
+}
+public abstract class Receiver
+{
+	//抽象接收者，定义每个接收者都必须完成的业务
+	public abstract void doSomething();
+}
+public class ConcreteReciver1 : Receiver
+{
+	//每个接受者都必须处理一定的业务逻辑
+	public override void doSomething()
+	{ Console.WriteLine("ConcreteReciver1 执行命令");	}
+}
+public class ConcreteReciver2 : Receiver
+{
+	//每个接受者都必须处理一定的业务逻辑
+	public override void doSomething()
+	{ Console.WriteLine("ConcreteReciver2 执行命令"); }
+}
+public class Invoker
+{
+	private Command command;
+	//受气包，接受命令
+	public void setCommand(Command _command)
+	{
+		this.command = _command;
+	}
+	//执行命令
+	public void action()
+	{
+		this.command.execute();
+	}
+}
+}
--- DesignPatterns/MediatorPattern/Program.cs
+++ DesignPatterns/MediatorPattern/Program.cs
@@ -6,10 +6,100 @@ using System.Threading.Tasks;
 
 namespace MediatorPattern
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ConcreteMediator concreteMediator = new ConcreteMediator();
+			ConcreteColleague1 colleague1 = new ConcreteColleague1(concreteMediator);
+			ConcreteColleague2 colleague2 = new ConcreteColleague2(concreteMediator);
+			concreteMediator.setC1(colleague1);
+			concreteMediator.setC2(colleague2);
+			concreteMediator.doSomething1();
+			concreteMediator.doSomething2();
+
+			concreteMediator.getC1().depMethod1();
+			concreteMediator.getC2().depMethod2();
+			Console.ReadLine();
+		}
+	}
+	public abstract class Mediator
+	{
+		//定义同事类
+		protected ConcreteColleague1 c1;
+		protected ConcreteColleague2 c2;
+
+		//通过getter/setter方法吧同事类注入进来
+		public ConcreteColleague1 getC1() { return c1; }
+		public void setC1(ConcreteColleague1 c1) { this.c1 = c1; }
+		public ConcreteColleague2 getC2() { return c2; }
+		public void setC2(ConcreteColleague2 c2) { this.c2 = c2; }
+
+		//中介者模式的业务逻辑
+		public abstract void doSomething1();
+		public abstract void doSomething2();
+	}
+	public class ConcreteMediator : Mediator
+	{
+
+		public override void doSomething1()
+		{
+			//调用同事类的方法，只要是public方法都可以调用
+			base.c1.selfMethod1();
+			base.c2.selfMethod2();
+		}
+
+		public override void doSomething2()
+		{
+			base.c1.selfMethod1();
+			base.c2.selfMethod2();
+		}
+
+	}
+	public abstract class Colleague
+	{
+		protected Mediator mediator;
+		public Colleague(Mediator _mediator)
+		{
+			this.mediator = _mediator;
+		}
+	}
+	public class ConcreteColleague1 : Colleague
+	{
+		//通过构造函数传递中介者
+		public ConcreteColleague1(Mediator _mediator) : base(_mediator)
+		{ }
+		//自有方法 self-method
+		public void selfMethod1()
+		{
+			Console.WriteLine("ConcreteColleague1 自有方法 selfMethod1");
+		}
+		//依赖方法 dep-method
+		public void depMethod1()
+		{
+			Console.WriteLine("ConcreteColleague1 依赖方法 depMethod1");
+			//自己不能处理的业务逻辑，委托给中介者处理
+			base.mediator.doSomething1();
+
+		}
+	}
+	public class ConcreteColleague2 : Colleague
+	{
+		//通过构造函数传递中介者
+		public ConcreteColleague2(Mediator _mediator) : base(_mediator)
+		{ }
+
+		//自有方法 self-method
+		public void selfMethod2()
+		{
+			Console.WriteLine("ConcreteColleague2 自有方法 selfMethod2");
+		}
+		//依赖方法 dep-method
+		public void depMethod2()
+		{
+			Console.WriteLine("ConcreteColleague2 依赖方法 depMethod2");
+			//自己不能处理的业务逻辑，委托给中介者处理
+			base.mediator.doSomething2();
+		}
+	}
 }
